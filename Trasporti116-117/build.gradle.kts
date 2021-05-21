f8c03cfcb596e4fc5a11ea455888cffce99a6002
Meshua Galassi <meshua.galassi@gmail.com>
2021-05-21 11:20:20 +0200

    updated build gradle and gitignore

--- Trasporti116-117/build.gradle.kts
+++ Trasporti116-117/build.gradle.kts
@@ -1,19 +1,15 @@
- plugins {
-     `java-gradle-plugin`
-     jacoco
-     id("org.jetbrains.kotlin.jvm") version "1.5.0-RC"
-     id("org.jetbrains.dokka") version "1.4.30"
-     id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.3"
-     id("com.gradle.plugin-publish") version "0.14.0"
-     id("pl.droidsonroids.jacoco.testkit") version "1.0.8"
-     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
-     id("io.gitlab.arturbosch.detekt") version "1.17.0-RC2"
+plugins {
+    jacoco     //java code coverage
+    id("java")  //javadoc
+    id("org.jetbrains.kotlin.jvm") version "1.5.0-RC"
+    id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.3"
+    id("pl.droidsonroids.jacoco.testkit") version "1.0.8"
+    id("io.gitlab.arturbosch.detekt") version "1.17.0-RC2"
 }
+
 gitSemVer{
     version = computeGitSemVer()
 }
-
-group = "org.meshuagalassi"
 
 repositories {
     mavenCentral()
@@ -21,6 +17,7 @@ repositories {
 
 dependencies {
     implementation(gradleApi()) // Implementation: available at compile and runtime, non transitive
+    implementation("com.puppycrawl.tools:checkstyle:8.42")
     implementation("com.azure:azure-digitaltwins-core:1.0.3")
     implementation("com.azure:azure-identity:1.2.4")
     implementation("org.slf4j:slf4j-api:1.7.30")
@@ -28,3 +25,16 @@ dependencies {
     implementation("org.apache.clerezza.ext:org.json.simple:0.4")
     implementation("com.azure:azure-core-http-okhttp:1.6.1")
 }
+
+tasks.jacocoTestReport{
+    reports{
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+}
+
+//tasks.javadoc {
+    //senza sorgente javadoc non crea nessuna documentazione
+    //source = sourceSets.main.allJava
+//}
+
