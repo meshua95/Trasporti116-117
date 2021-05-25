5315fcc556fefdf5929a3927826cb18fa39f4d05
giada <>
2021-05-25 17:14:20 +0200

    add create trasporto GUI

---
+++ Trasporti116-117/src/main/java/domain/ambulanza/IdAmbulanza.java
@@ -0,0 +1,13 @@
+package domain.ambulanza;
+
+public class IdAmbulanza {
+    private String IdAmbulanza;
+
+    public IdAmbulanza(String idAmbulanza) {
+        IdAmbulanza = idAmbulanza;
+    }
+
+    public String getIdAmbulanza() {
+        return IdAmbulanza;
+    }
+}
---
+++ Trasporti116-117/src/main/java/domain/operatore/IdOperatore.java
@@ -0,0 +1,13 @@
+package domain.operatore;
+
+public class IdOperatore {
+    private String codiceFiscale;
+
+    public IdOperatore(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+}
