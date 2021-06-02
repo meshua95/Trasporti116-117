51cef9c910187befeaf7482922aa6bfd0b17e93f
Meshua Galassi <meshua.galassi@gmail.com>
2021-06-02 12:09:42 +0200

    added idAmbulance test

---
+++ Trasporti116-117/src/test/java/AmbulanceIdGeneration.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021. Galassi Meshua, Gibertoni Giada
+ */
+
+import model.AmbulanceId;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AmbulanceIdGeneration {
+    private int ambulanceNumber = 2;
+
+    @Test
+    public void createAmbulanceIdByAmbulanceNumber(){
+        AmbulanceId ambulanceId = new AmbulanceId(ambulanceNumber);
+        assertEquals(ambulanceId.getAmbulanceId(), "ambulanza-"+ambulanceNumber);
+    }
+
+    @Test
+    public void createGpsIdByAmbulanceNumber(){
+        AmbulanceId ambulanceId = new AmbulanceId(ambulanceNumber);
+        assertEquals(ambulanceId.getGpsId(), "gps-"+ambulanceNumber);
+    }
+
+    @Test
+    public void createGpsIdByAmbulanceId(){
+        AmbulanceId ambulanceId = new AmbulanceId("ambulanza-"+ambulanceNumber);
+        assertEquals(ambulanceId.getGpsId(), "gps-"+ambulanceNumber);
+    }
+
+    @Test
+    public void checkGetAmbulanceNumber(){
+        AmbulanceId ambulanceId = new AmbulanceId("ambulanza-"+ambulanceNumber);
+        assertEquals(ambulanceId.getAmbulanceNumber(), ambulanceNumber);
+    }
+
+}
