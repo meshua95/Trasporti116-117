a633e18ea3d0235743cd58e1aceb372d3b7209de
Meshua Galassi <meshua.galassi@gmail.com>
2021-06-09 17:27:47 +0200

    fixed bug in getAllBookingForTheDay

--- Trasporti116-117/src/main/java/Main.java
+++ Trasporti116-117/src/main/java/Main.java
@@ -2,10 +2,12 @@
  * Copyright (c) 2021. Galassi Meshua, Gibertoni Giada
  */
 
+import digitalTwins.booking.BookingDigitalTwin;
 import view.MainApp;
 
 public class Main {
     public static void main(String... arg) {
-        MainApp.main(arg);
+        //MainApp.main(arg);
+        BookingDigitalTwin.getAllBookingForToday();
     }
 }
