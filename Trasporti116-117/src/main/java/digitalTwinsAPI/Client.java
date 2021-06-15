8d104b8cc620e924c75e4e74bc05194501a925b5
giada <>
2021-06-15 18:43:49 +0200

    minor change

--- Trasporti116-117/src/main/java/digitalTwinsAPI/Client.java
+++ Trasporti116-117/src/main/java/digitalTwinsAPI/Client.java
@@ -4,17 +4,12 @@
 
 package digitalTwinsAPI;
 
-import com.azure.digitaltwins.core.DigitalTwinsAsyncClient;
 import com.azure.digitaltwins.core.DigitalTwinsClient;
 import com.azure.digitaltwins.core.DigitalTwinsClientBuilder;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import utils.Constants;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Client {
-    public static ExecutorService executor = Executors.newSingleThreadExecutor();
     private static DigitalTwinsClient client = null;
 
     private static void createClient(){
@@ -38,26 +33,4 @@ public class Client {
         return client;
     }
 
-    private static DigitalTwinsAsyncClient clientAsync = null;
-
-    private static void createClientAsync(){
-        // Create client connection
-        clientAsync = new DigitalTwinsClientBuilder()
-                .credential(
-                        new ClientSecretCredentialBuilder()
-                                .tenantId(Constants.TENANT_ID)
-                                .clientId(Constants.CLIENT_ID)
-                                .clientSecret(Constants.CLIENT_SECRET)
-                                .build()
-                )
-                .endpoint(Constants.ENDPOINT)
-                .buildAsyncClient();
-    }
-
-    public static DigitalTwinsAsyncClient getClientAsync(){
-        if (clientAsync == null) {
-            createClientAsync();
-        }
-        return clientAsync;
-    }
 }
